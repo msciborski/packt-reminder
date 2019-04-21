a298779f0f0f92569da86736bf693617e1d60185
msciborski <michal.sciborski@student.put.poznan.pl>
2019-04-21 09:21:36 +0200

    validation

--- Server/src/authentication/authentication.controller.ts
+++ Server/src/authentication/authentication.controller.ts
@@ -1,7 +1,7 @@
 import * as bcrypt from 'bcrypt';
 import * as express from 'express';
 import Controller from "../interfaces/controller.interface";
-import userModel from '../users/user.model';
+import userModel from '../user/user.model';
 
 class AuthenticationController implements Controller {
   public path = '/auth';
