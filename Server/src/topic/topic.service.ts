934b2423cb1b623704af1d03d6fe0821f880e8f5
msciborski <michal.sciborski@student.put.poznan.pl>
2019-04-22 14:35:38 +0200

    Change way of inserting multiple topics

--- Server/src/topic/topic.service.ts
+++ Server/src/topic/topic.service.ts
@@ -8,23 +8,17 @@ class TopicService {
   public user = userModel;
 
   public async addTopics(topics: string[], userId: string) {
-    const unifiedTopics = topics.map(topic => topic.toLowerCase());
-
-    const topicsAddedToDb: Topic[] = [];
-
-    for (const topic in unifiedTopics) {
-      const addedTopic = await this.topic.create({ name: topic });
-      topicsAddedToDb.push(addedTopic);
-    }
+    const unifiedTopics = topics.map(topic => ({ name: topic.toLowerCase() }));
+    const addedTopics = await this.topic.insertMany(unifiedTopics);
 
     const user = await this.user.findById(userId);
 
     if (user) {
-      const topicIds = topicsAddedToDb.map(topic => topic.id);
+      const topicIds = addedTopics.map(topic => topic.id);
       user.topics.push(...topicIds);
       await user.save();
 
-      return user;
+      return await user.populate('topics').execPopulate();
     } else {
       throw new HttpException(400, 'User not found.');
     }
