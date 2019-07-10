dda46d28ef6d5449f48ce1e77bf4a92df760392e
msciborski <michal.sciborski@student.put.poznan.pl>
2019-07-10 18:44:20 +0200

    Add mechanism for scheduling jobs

--- Jobs/src/index.js
+++ Jobs/src/index.js
@@ -1,19 +1,31 @@
 const scrapper = require('./scrapper');
 const dotenv = require('dotenv').config();
 const userApi = require('./userApi');
+const schedule = require('node-schedule');
+
+const url = 'https://www.packtpub.com/free-learning';
 
 const notifyUsers = async () => {
   const bookInfo = await scrapper.getBookInformation();
   const users = await userApi.getUsersWithTokens();
 }
 
+const rule = schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 6;
+rule.minute = 0;
 
-(async () => {
-  try {
-    const users = await userApi.getUsersWithTokens();
-    console.log(users);
-  } catch (error) {
-    console.log(error);
-  }
+var job = schedule.scheduleJob(rule, async () => {
+  const bookInfo = await scrapper.getBookInformation(url);
+});
 
-})();
+
+// (async () => {
+//   try {
+//     const users = await userApi.getUsersWithTokens();
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// })();
